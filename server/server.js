1225fdc02dd66beada1c857720fc735a217aeb32
Ananya Singh <ananyasinghfl21@gmail.com>
2024-08-25 12:21:48 +0530

    Initial commit

---
+++ server/server.js
@@ -0,0 +1,54 @@
+// const express = require('express')
+// const app = express ()
+// app.get ("/api", (req, res) => {
+//           res. json({ "users": ["userOne", "userTwo", "userThree"] })
+// })
+
+// app.listen(5000, () => { console. log("Server started on port 5000") })
+
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+
+const userId = 'john_doe_17091999';
+const email = 'john@xyz.com';
+const rollNumber = 'ABCD123';
+
+app.post('/bfhl', (req, res) => {
+  const data = req.body.data;
+  const numbers = [];
+  const alphabets = [];
+  let highestLowercaseAlphabet = [];
+
+  data.forEach((item) => {
+    if (!isNaN(item)) {
+      numbers.push(item);
+    } else if (item.match(/[a-z]/i)) {
+      alphabets.push(item);
+      if (item.toLowerCase() === item) {
+        highestLowercaseAlphabet = [item];
+      }
+    }
+  });
+
+  res.json({
+    is_success: true,
+    user_id: userId,
+    email,
+    roll_number: rollNumber,
+    numbers,
+    alphabets,
+    highest_lowercase_alphabet: highestLowercaseAlphabet,
+  });
+});
+
+app.get('/bfhl', (req, res) => {
+  res.json({ operation_code: 1 });
+});
+
+const port = 5000;
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
+});
